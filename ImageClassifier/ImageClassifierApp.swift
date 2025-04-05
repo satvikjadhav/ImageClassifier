cf927af213b0f33577eb96d90bbd6ef0ac04fb0b
satvikjadhav <satvikj5@gmail.com>
2025-04-04 23:45:40 -0500

    Initial Commit

---
+++ ImageClassifier/ImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassifierApp.swift
+//  ImageClassifier
+//
+//  Created by Satvik  Jadhav on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
